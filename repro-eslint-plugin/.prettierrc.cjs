01c0dde1107bd492444dee3f7b1f0e89d9e20ec8
void_malex <void-mAlex@users.noreply.github.com>
2024-11-26 11:09:23 +0000

    show eslint-plugin-ember failures in app code

--- repro-eslint-plugin/.prettierrc.cjs
+++ repro-eslint-plugin/.prettierrc.cjs
@@ -1,6 +1,26 @@
 'use strict';
 
 module.exports = {
-  plugins: ['prettier-plugin-ember-template-tag'],
-  singleQuote: true,
+	plugins: ['prettier-plugin-ember-template-tag'],
+	singleQuote: true,
+	useTabs: true,
+	tabWidth: 4,
+	overrides: [
+		{
+			files: '*.{js,ts,gjs,gts}',
+			options: {
+				templateExportDefault: true,
+				templateSingleQuote: false,
+				tabWidth: 2,
+			},
+		},
+		{
+			files: '*.hbs',
+			options: {
+				parser: 'glimmer',
+				singleQuote: false,
+				tabWidth: 2,
+			},
+		},
+	],
 };
--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -3,12 +3,23 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {
-    autoImport: {
-      watchDependencies: ['repro-eslint-plugin'],
-    },
-  });
+	let app = new EmberApp(defaults, {
+		autoImport: {
+			watchDependencies: ['repro-eslint-plugin'],
+		},
+		'ember-cli-babel': {
+			// turn off the old transform
+			// (for this to work when using Embroider you need https://github.com/embroider-build/embroider/pull/1673)
+			disableDecoratorTransforms: true,
+		},
+		babel: {
+			plugins: [
+				// add the new transform.
+				require.resolve('decorator-transforms'),
+			],
+		},
+	});
 
-  const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app);
+	const { maybeEmbroider } = require('@embroider/test-setup');
+	return maybeEmbroider(app);
 };
